d1e78c953c666f5a0a9c3dba915723a47e03aaa0
pavin <fellipe306@gmail.com>
2025-11-02 17:39:17 -0300

    create and read

--- src/index.js
+++ src/index.js
@@ -8,19 +8,54 @@ const PORT = 3000;
 const pool = new Pool({
 	user: "admin",
 	host: "localhost",
-	database: "postgres",
+	database: "expense_analyzer_db",
 	password: "Abacaxi@123",
 	port: 5432,
 });
 
-app.get("/", async (req, res) => {
-	try {
-		const result = await pool.query("SELECT NOW()");
+app.use(express.json());
 
-		res.json(result.rows[0]);
+app.get("/", (req, res) => {
+	res.send("Expense analyzer api is online!");
+});
+
+app.post("/expenses", async (req, res) => {
+	const { description, amount, category } = req.body;
+
+	if (!description || !amount) {
+		return res
+			.status(400)
+			.json({ error: "Description and amount are required." });
+	}
+
+	try {
+		const sqlQuery = `
+  INSERT INTO expenses (description, amount, category)
+  VALUES($1, $2, $3)
+  RETURNING *;
+`;
+
+		const values = [description, amount, category || null];
+
+		const result = await pool.query(sqlQuery, values);
+
+		res.status(201).json(result.rows[0]);
+	} catch (error) {
+		console.error("Error trying to insert expense:", error);
+		res.status(500).json({ error: "Intern server error." });
+	}
+});
+
+app.get("/expenses", async (req, res) => {
+	try {
+		const sqlQuery = "SELECT * FROM expenses ORDER BY created_at DESC;";
+
+		const result = await pool.query(sqlQuery);
+
+		res.status(200).json(result.rows);
 	} catch (error) {
 		console.error("Connection or database query error!", error);
-		res.status(500).send("Error connecting database.");
+		res.status(500).json({ error: "Intern error." });
 	}
 });
 
